30c4ad7dd3f4eb4e72698d1409640a602024c0ba
evanhackstadt <evan.hackstadt@gmail.com>
2025-12-23 00:40:25 -0800

    Add select Snakemake files

---
+++ snakemake-testing/submit_snakemake.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#SBATCH --job-name=Snakemake_Master
+#SBATCH --output=out-snakemake-master-%j.out 
+#
+#SBATCH --partition=cmp
+#SBATCH --nodes=1 
+#SBATCH --ntasks=1 
+#SBATCH --cpus-per-task=1   # Cores for the Controller
+#SBATCH --mem-per-cpu=4G    # Memory for the Controller
+#SBATCH --time=24:00:00     # Max time for the Controller
+#
+#SBATCH --mail-user=ehackstadt@scu.edu
+#SBATCH --mail-type=END
+#
+set -e
+echo "Snakemake master wrapper script started at $(date)"
+#
+# 1. Load and activate conda environment where Snakemake is installed
+module load Anaconda3
+conda activate /WAVE/projects/whittalllab/conda_envs/genomics
+#
+# 2. Run the main Snakemake command
+# All args specified in profiles/config.yaml (including executor: slurm)
+snakemake --profile "profiles/"
